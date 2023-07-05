216bae260689507489f00c45408db863079f5412
Clement Guyon <clement.guyon@ynov.com>
2023-07-05 16:12:27 +0200

    feature: mqtt

--- TempSensorIOT_GUYON/src/main.cpp
+++ TempSensorIOT_GUYON/src/main.cpp
@@ -4,6 +4,16 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <ArduinoBLE.h>
+#include <EEPROM.h>
+
+#define EEPROM_SIZE 512
+#define CONFIG_ADDRESS 0
+
+// Variables de configuration
+int tempFreq = 5; // Fréquence d'acquisition de la température en secondes
+int connectionFreq = 5; // Fréquence d'envoi des données par WiFi en secondes
+int connectionConfig = 1; // Protocole de connexion (1: HTTP, 2: MQTT)
+
 
 const char *ssid = "Clement";
 const char *password = "ClementG69";
@@ -11,11 +21,11 @@ const char *serverName = "http://172.20.10.2:3000/";
 
 HTTPClient http;
 WiFiClient client;
-String configuration;
 
 bool isBLEInitialized;
 bool oldDeviceConnected = false;
 uint8_t value = 0;
+String configuration;
 
 // UUID du service et de la caractéristique
 const char SERVICE_UUID[] = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
@@ -23,14 +33,20 @@ const char CHARACTERISTIC_UUID[] = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 
 BLEService bleService(SERVICE_UUID);
 BLEByteCharacteristic bleCharacteristic(CHARACTERISTIC_UUID, BLERead | BLEWrite | BLENotify);
+PubSubClient mqttClient(client);
+
+// Variables pour la communication MQTT
+const char* mqttBroker = "test.mosquitto.org";
+const int mqttPort = 1883;
+const char* mqttClientId = "espClem";
+const char* mqttTopic = "ynov-lyon-2023/esp32/in";
+const char* espId = "Esp32IdUnique";
 
 void initializeBLE()
 {
   if (!BLE.begin())
   {
     Serial.println("starting BLE failed!");
-    while (1)
-      ;
   }
 
   BLE.setLocalName("ESP32_BLE");
@@ -48,6 +64,7 @@ void initializeBLE()
 
 void receiveValueBLE()
 {
+  Serial.println("BLE ready to receive data...");
   BLEDevice central = BLE.central();
 
   if (central)
@@ -66,9 +83,9 @@ void receiveValueBLE()
 
   if (central.connected())
   {
-    // Attendez que les données soient écrites
     while (!bleCharacteristic.written())
     {
+      Serial.println("En attente d'écriture");
       delay(10); // évite une boucle infinie consommant trop de ressources
     }
 
@@ -77,8 +94,9 @@ void receiveValueBLE()
     value++;
     delay(10);
   }
-}
 
+  BLE.end();
+}
 
 void sendTemperature(float temperature)
 {
@@ -166,7 +184,7 @@ String getConfiguration()
       Serial.print(F("deserializeJson() a échoué : "));
       return error.f_str();
     }
-    
+
     Serial.println(response);
     if (doc.containsKey("type"))
     {
@@ -176,7 +194,9 @@ String getConfiguration()
       Serial.print("CONFIGURATION GET: ");
       Serial.println(sensor);
       return String(sensor);
-    } else {
+    }
+    else
+    {
       Serial.println("Le champ TYPE n'a pas été trouvé");
     }
 
@@ -196,18 +216,83 @@ String getConfiguration()
   WiFi.mode(WIFI_OFF);
 }
 
+void readConfigFromEEPROM() {
+  EEPROM.begin(EEPROM_SIZE);
+
+  // Lecture de la configuration depuis l'EEPROM
+  EEPROM.get(0, tempFreq);
+  EEPROM.get(1, connectionConfig);
+  EEPROM.get(2, connectionFreq);
+
+  EEPROM.end();
+}
+
+// Méthode pour sauvegarder la configuration dans la mémoire EPROM
+void saveConfigToEEPROMIfEmpty() {
+  EEPROM.begin(EEPROM_SIZE);
+
+  // Vérifier si l'EEPROM est vide en lisant le premier octet
+  byte firstByte = EEPROM.read(CONFIG_ADDRESS);
+  if (firstByte != 0xFF) {
+    // L'EEPROM n'est pas vide, la configuration existe déjà
+    EEPROM.end();
+    return;
+  }
+
+  // L'EEPROM est vide, sauvegarde de la configuration
+  EEPROM.put(1, tempFreq);
+  EEPROM.put(2, connectionConfig);
+  EEPROM.put(3, connectionFreq);
+
+  EEPROM.commit();
+  EEPROM.end();
+}
+
+// Méthode pour envoyer les données via MQTT
+void sendDataToMQTT() {
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    if (mqttClient.connect(mqttClientId))
+    {
+      // Construction du message JSON
+      DynamicJsonDocument jsonBody(256);
+      jsonBody["body"]["config"]["tempFreq"] = EEPROM.get(0, tempFreq);
+      jsonBody["body"]["config"]["connectionConfig"] = connectionConfig;
+      jsonBody["body"]["config"]["connectionFreq"] = connectionFreq;
+      jsonBody["temperature"] = readTemp2(false);
+      jsonBody["id"] = espId;
+      String message;
+      serializeJson(jsonBody, message);
+      Serial.println(message.c_str());
+      mqttClient.publish(mqttTopic, message.c_str());
+        mqttClient.beginPublish(mqttTopic, 1, false);
+          mqttClient.endPublish();
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("------------------------");
+  configuration = getConfiguration();
+  Serial.println("------------------------");
+
   delay(300);
 }
 
 void loop()
 {
   float temp = readTemp2();
-  Serial.println("------------------------");
-  configuration = getConfiguration();
-  Serial.println("------------------------");
 
   if (configuration.compareTo("http") == 0)
   {
@@ -229,7 +314,12 @@ void loop()
 
     if (!isBLEInitialized)
     {
+      Serial.println("BLE initialization");
       initializeBLE();
+    }
+    else
+    {
+      Serial.println("BLE already initialized");
     }
     receiveValueBLE();
   }
